6d4999b27b2b0b9d17d399b92df788da4f052c48
Vamsi Anamalamudi <vamsi.anamalamudi@gmail.com>
2026-09-10 09:14:37 +0530

    A-Note-of-Thanks-hans

--- data/people.ts
+++ data/people.ts
@@ -195,7 +195,7 @@ export const defaultPeople: Person[] = [
     id: 'Reeshna-Hans',
     name: 'Reeshna & Hans',
     message:
-      '​First off, huge thanks for saving the day during my first outage on a Friday night! :p \n Beyond the fire-fighting, I truly appreciate your endless patience in answering all my silly doubts and guiding me through my journey at PhonePe. I learned a lot from both of you. Wishing you all the best, and I really hope our paths cross again!',
+      '​First off, huge thanks for saving the day during my first outage on a Friday night! :p \n Beyond the fire-fighting, I truly appreciate your endless patience in answering all my silly doubts and guiding me through my journey at PhonePe. I learned a lot from both of you. I really hope our paths cross again!',
     insideJoke: '',
     enabled: true,
   },
